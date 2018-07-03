0096aabd5be541cbcee9a67e211f8941ce2a3a4d
Reeti <reeti.banthia@utexas.edu>
2018-07-03 01:09:10 -0700

    Add impl of regex, index, and timer. Missing: readme, more tests

--- src/DocumentSearch.java
+++ src/DocumentSearch.java
@@ -1,12 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocumentSearch {
 	static String term;
 	static int search;
 	static ArrayList<FileRelevency> docs = new ArrayList<FileRelevency>();
+	Scanner s = new Scanner(System.in);
+	Long startTime = (long) 0;
+	Long endTime = (long) 0;
 
 	public static void main(String[] args) {
 		DocumentSearch d = new DocumentSearch();
@@ -23,27 +27,27 @@ public class DocumentSearch {
 			if (listOfFiles[i].isFile() && listOfFiles[i].toString().contains(".txt")) {
 				docs.add(new FileRelevency(listOfFiles[i]));
 				System.out.println("File " + listOfFiles[i].getName());
-			} 
+			}
 		}
 	}
 
 	@SuppressWarnings("resource")
 	public void prompt() {
-		Scanner scan = new Scanner(System.in);
+		s = new Scanner(System.in);
 		System.out.print("Enter the search term: ");
-		term = " "+scan.nextLine().toLowerCase();
-		if(term.lastIndexOf(' ') != term.length()-1) {
-			term+=" ";
+		term = " " + s.nextLine().toLowerCase();
+		if (term.lastIndexOf(' ') != term.length() - 1) {
+			term += " ";
 		}
 		int search;
 		do {
 			System.out.print(
 					"\nSearch Method? \n1) String Match\n2) Regular Expression\n3) Indexed. \nEnter 1,2, or 3:  ");
-			while (!scan.hasNextInt()) {
+			while (!s.hasNextInt()) {
 				System.out.print("\nPlease enter a valid search method choice: ");
-				scan.next();
+				s.next();
 			}
-			search = scan.nextInt();
+			search = s.nextInt();
 		} while (search < 1 && search > 3);
 
 		switch (search) {
@@ -61,46 +65,76 @@ public class DocumentSearch {
 	}
 
 	public void stringMatch() {
-		Scanner s=new Scanner(System.in);
-		String temp="hello";
-		for(FileRelevency fr: docs) {
+		startTime = System.currentTimeMillis();
+		s = new Scanner(System.in);
+		String temp = "";
+		for (FileRelevency fr : docs) {
 			try {
-				s =  new Scanner(fr.getFile());
+				s = new Scanner(fr.getFile());
+				while (s.hasNextLine()) {
+					temp = " " + s.nextLine().toLowerCase().replaceAll("\\p{Punct}", " ");
+					while (temp.indexOf(term) != -1) {
+						fr.addCount();
+						temp = temp.substring(temp.indexOf(term) + term.length() - 1);
+					}
+				}
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
-			while(s.hasNextLine()) {
-				temp = " "+ s.nextLine().toLowerCase().replaceAll("\\p{Punct}", "");
-				while(temp.indexOf(term) != -1) {
-					fr.addCount();
-					temp = temp.substring(temp.indexOf(term)+term.length()-1);
-//					System.out.println(temp);
-				}
-			}
-		}		
+		}
+		endTime = System.currentTimeMillis();
 	}
 
 	public void regEx() {
-
+		startTime = System.currentTimeMillis();
+		s = new Scanner(System.in);
+		String temp = "";
+		Pattern p = Pattern.compile(term.trim());
+		for (FileRelevency fr : docs) {
+			try {
+				s = new Scanner(fr.getFile());
+				if (s.hasNextLine()) {
+					temp = s.useDelimiter("\\Z").next().toLowerCase();
+					Matcher m = p.matcher(temp);
+					int start = 0;
+					try {
+						while (m.find(start)) {
+							fr.addCount();
+							start = m.start() + 1;
+						}
+					} catch (Exception e) {
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		endTime = System.currentTimeMillis();
 	}
 
 	public void indexed() {
-
+		startTime = System.currentTimeMillis();
+		for (FileRelevency fr : docs) {
+			fr.createIndex();
+			fr.getInstances(term.toLowerCase());
+		}
+		endTime = System.currentTimeMillis();
 	}
-	
+
 	public class CustomComparator implements Comparator<FileRelevency> {
-	    @Override
+		@Override
 		public int compare(FileRelevency o1, FileRelevency o2) {
 			return o2.getCount() - o1.getCount();
 		}
 	}
-	
+
 	public void printSorted() {
 		System.out.println();
 		Collections.sort(docs, new CustomComparator());
-		for(FileRelevency fr: docs) {
+		for (FileRelevency fr : docs) {
 			System.out.format("%-40s%-40d\n", fr.getName(), fr.getCount());
 		}
+		System.out.format("Elapsed Time: %10d", endTime - startTime);
 	}
 
 }
--- src/FileRelevency.java
+++ src/FileRelevency.java
@@ -1,29 +1,66 @@
 import java.io.File;
-import java.util.Comparator;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class FileRelevency {
 	File file;
-	int count;
-	
+	Integer count;
+	Map<String, Integer> index = new HashMap<String, Integer>();
+
 	public FileRelevency(File inputFile) {
 		this.file = inputFile;
-		this.count=0;
+		this.count = 0;
 	}
-	
+
 	public File getFile() {
 		return file;
 	}
-	
+
 	public String getName() {
 		return file.getName();
 	}
-	
+
 	public int getCount() {
 		return count;
 	}
-	
+
+	public void setCount(Integer i) {
+		count = i;
+	}
+
 	public void addCount() {
 		count++;
 	}
-	
+
+	public void getInstances(String term) {
+		try {
+			Integer val = this.index.get(term.trim());
+			setCount(val);
+		} catch (Exception e) {
+			count = 0;
+		}
+
+	}
+
+	public void createIndex() {
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		String temp = new String();
+		Integer num = 0;
+		try {
+			s = new Scanner(getFile());
+			while (s.hasNext()) {
+				temp = s.next().toLowerCase().replaceAll("\\p{Punct}", "");
+				if (index.get(temp) != null) {
+					num = index.get(temp);
+					this.index.put(temp, num + 1);
+				} else {
+					this.index.put(temp, 1);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
