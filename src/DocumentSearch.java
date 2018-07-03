2b58dd454f85be14188ed1bb9f16d92dce0fcc86
Reeti <reeti.banthia@utexas.edu>
2018-07-02 19:28:03 -0700

    indexSearch - first

--- src/DocumentSearch.java
+++ src/DocumentSearch.java
@@ -1,10 +1,11 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class DocumentSearch {
 	static String term;
 	static int search;
-	static ArrayList<FileRelevency> docs = new ArrayList<File>();
+	static ArrayList<FileRelevency> docs = new ArrayList<FileRelevency>();
 
 	public static void main(String[] args) {
 		DocumentSearch d = new DocumentSearch();
@@ -18,7 +19,7 @@ public class DocumentSearch {
 		System.out.println("Searching from the following files: ");
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile() && listOfFiles[i].toString().contains(".txt")) {
-				docs.add(listOfFiles[i]);
+				docs.add(new FileRelevency(listOfFiles[i]));
 				System.out.println("File " + listOfFiles[i].getName());
 			} 
 		}
@@ -28,7 +29,10 @@ public class DocumentSearch {
 	public void prompt() {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the search term: ");
-		term = scan.next();
+		term = " "+scan.nextLine().toLowerCase();
+		if(term.lastIndexOf(' ') != term.length()-1) {
+			term+=" ";
+		}
 		int search;
 		do {
 			System.out.print(
@@ -55,7 +59,26 @@ public class DocumentSearch {
 	}
 
 	public void stringMatch() {
-		
+		Scanner s=new Scanner(System.in);
+		String temp="hello";
+		for(FileRelevency fr: docs) {
+			try {
+				s =  new Scanner(fr.getFile());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			while(s.hasNextLine()) {
+				temp = " "+ s.nextLine().toLowerCase().replaceAll("\\p{Punct}", "");
+				while(temp.indexOf(term) != -1) {
+					fr.addCount();
+					temp = temp.substring(temp.indexOf(term)+term.length()-1);
+					System.out.println(temp);
+				}
+			}
+		}
+		for(FileRelevency fr: docs) {
+			System.out.println(fr.getFile().getName() + "  "+ fr.getCount());
+		}
 		
 	}
 
@@ -65,6 +88,10 @@ public class DocumentSearch {
 
 	public void indexed() {
 
+	}
+	
+	public void printSorted() {
+		
 	}
 
 }
--- src/FileRelevency.java
+++ src/FileRelevency.java
@@ -1,9 +1,24 @@
 import java.io.File;
 
 public class FileRelevency {
-
-	public FileRelevency(File f, int count) {
-		
+	File file;
+	int count;
+	
+	public FileRelevency(File inputFile) {
+		this.file = inputFile;
+		this.count=0;
+	}
+	
+	public File getFile() {
+		return file;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public void addCount() {
+		count++;
 	}
 
 }
